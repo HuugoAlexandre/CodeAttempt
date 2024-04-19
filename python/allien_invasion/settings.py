1b66461eea29712a8caed0bd24ac4a373526719f
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-04-19 17:17:36 -0300

    Adição da capacidade de tiro

--- python/allien_invasion/settings.py
+++ python/allien_invasion/settings.py
@@ -3,4 +3,11 @@ class Settings():
         self.screen_width = 900
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        
         self.ship_speed_factor = 0.6
+
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
