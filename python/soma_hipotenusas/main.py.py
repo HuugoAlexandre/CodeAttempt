0eeefb16c70dc7aad09dfa529e7d5108c36a54d6
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-04-29 20:51:09 -0300

    Soma de hipotenusas

---
+++ python/soma_hipotenusas/main.py.py
@@ -0,0 +1,16 @@
+def soma_hipotenusa(n):
+    soma = 0
+    for i in range(1, n + 1):
+        if é_hipotenusa(i):
+            soma += i
+    return soma
+
+def é_hipotenusa(n):
+    for a in range(1, n + 1):
+        for b in range(a, n + 1 ):
+            if a ** 2 + b ** 2 == n ** 2:
+                return True
+    return False
+
+hi = int(input('Numero: '))
+print(soma_hipotenusa(25))
