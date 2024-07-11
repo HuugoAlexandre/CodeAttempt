042643a559adc4d0853d6617c8d2e10a4030b0c0
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-07-11 15:33:53 -0300

    Operações básicas de soma e subtração

---
+++ assembly/mips/syntax/add_sub_operations.asm
@@ -0,0 +1,14 @@
+.text
+	li $t0, 100
+	li $t1, 40
+	add $s0, $t0, $t1
+	addi $t2, $zero, 30	# funciona como li
+	addi $s1, $s0, 25
+	
+	li $t3, 50
+	li $t4, 44
+	li $t6, -10
+	sub $s3, $t3, $t4
+	subi $t5, $zero, 80
+	subi $s5, $s0, 20
+	subi $s6, $t6, -30
